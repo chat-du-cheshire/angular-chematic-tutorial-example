1703ac990f598db98277513524e51ed625af2043
Max Ivanov <maxlvan@yandex.ru>
2019-04-12 20:53:27 +0300

    9. Testing schematics

--- src/hello/index_spec.ts
+++ src/hello/index_spec.ts
@@ -1,16 +1,23 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
+import * as assert from "assert";
 
 
 const collectionPath = path.join(__dirname, '../collection.json');
+const runner = new SchematicTestRunner('schematics', collectionPath);
 
+let appTree: UnitTestTree;
 
-describe('hello', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('hello', {}, Tree.empty());
+describe('Schematic: Hello', () => {
+  beforeEach(() => {
+    appTree = runner.runExternalSchematic('@schematics/angular', 'workspace', {});
+    appTree = runner.runExternalSchematic('@schematics/angular', 'application', {}, appTree);
+  });
 
-    expect(tree.files).toEqual([]);
+  it('should create hello component', () => {
+    const options = {name: 'Max'};
+    const tree = runner.runSchematic('hello', options, appTree);
+
+    assert(tree.files.includes('/projects/bar/src/app/hello-max/hello-max.component.ts'));
   });
 });
