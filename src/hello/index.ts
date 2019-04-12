58449c1dcb4eaaf9a6eaf32e3b6f81999568621b
Max Ivanov <maxlvan@yandex.ru>
2019-04-12 20:41:37 +0300

    8. Integrate custom schematics with Angular CLI

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -1,20 +1,42 @@
-import {apply, mergeWith, Rule, SchematicContext, template, Tree, url} from '@angular-devkit/schematics';
+import {
+    apply,
+    mergeWith, move,
+    Rule,
+    SchematicContext,
+    SchematicsException,
+    template,
+    Tree,
+    url
+} from '@angular-devkit/schematics';
 import {Schema} from './schema';
 import {strings} from '@angular-devkit/core';
-
-
-function addExclamation(value: string): string {
-    return `${value}!`;
-}
+import {buildDefaultPath} from "@schematics/angular/utility/project";
+import {parseName} from "@schematics/angular/utility/parse-name";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function hello(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
+        const workspaceConfigBuffer = tree.read('angular.json');
+
+        if (!workspaceConfigBuffer) {
+            throw new SchematicsException('Not an Angular CLI workspace');
+        }
+
+        const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+        const projectName = _options.project || workspaceConfig.defaultProject;
+        const project = workspaceConfig.projects[projectName];
+
+        const defaultProjectPath = buildDefaultPath(project);
+
+        const parsedPath = parseName(defaultProjectPath, _options.name);
+
+        const {name, path} =  parsedPath;
 
         const sourceTemplates = url('./files');
         const sourceParametrizedTemplates = apply(sourceTemplates, [
-            template({..._options, ...strings, addExclamation})
+            template({..._options, ...strings, name}),
+            move(path)
         ]);
 
         return mergeWith(sourceParametrizedTemplates)(tree, _context);
